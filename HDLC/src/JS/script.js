60b88d73e0d1cc470401883d6fd0e69b81ba36b1
clxsrdev <99055585+clxsrdev@users.noreply.github.com>
2022-11-14 19:23:22 -0600

    Update script.js

--- HDLC/src/JS/script.js
+++ HDLC/src/JS/script.js
@@ -22,3 +22,42 @@ const body = document.querySelector("body"),
             modeText.innerText = "Dark Mode"
         }
       });
+
+const container = document.querySelector(".iupac");
+
+const home = document.querySelector(".home"),
+      citas = document.querySelector(".citas");
+
+const infoDocs = document.querySelector(".info-docs"),
+      overlay = document.querySelector(".overlay"),
+      cerrarBtn = document.querySelector(".cerrar-btn"),
+      aboutMeBtn = document.querySelector(".aboutMe");
+
+      aboutMeBtn.addEventListener("click", () => infoDocs.classList.add("active"));
+      cerrarBtn.addEventListener("click", () => infoDocs.classList.remove("active"));
+      overlay.addEventListener("click", () => infoDocs.classList.remove("active"));
+
+const imenu = document.querySelector(".Imenu"),
+      icitas = document.querySelector(".Icitas"),
+      icitasprox = document.querySelector(".Icitas-prox"),
+      iperfil = document.querySelector(".Iperfil");
+
+      imenu.addEventListener("click", () => container.classList.add("menu"));
+      imenu.addEventListener("click", () => container.classList.remove("cita"));
+      imenu.addEventListener("click", () => container.classList.remove("ctpr"));
+      imenu.addEventListener("click", () => container.classList.remove("perfil"));
+
+      icitas.addEventListener("click", () => container.classList.add("cita"));
+      icitas.addEventListener("click", () => container.classList.remove("menu"));
+      icitas.addEventListener("click", () => container.classList.remove("ctpr"));
+      icitas.addEventListener("click", () => container.classList.remove("perfil"));
+
+      icitasprox.addEventListener("click", () => container.classList.add("ctpr"));
+      icitasprox.addEventListener("click", () => container.classList.remove("menu"));
+      icitasprox.addEventListener("click", () => container.classList.remove("cita"));
+      icitasprox.addEventListener("click", () => container.classList.remove("perfil"));
+
+      iperfil.addEventListener("click", () => container.classList.add("perfil"));
+      iperfil.addEventListener("click", () => container.classList.remove("menu"));
+      iperfil.addEventListener("click", () => container.classList.remove("cita"));
+      iperfil.addEventListener("click", () => container.classList.remove("ctpr"));
