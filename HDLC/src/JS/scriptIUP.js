2ba9478965154dc429a847cd90922875914f5904
clxsrdev <99055585+clxsrdev@users.noreply.github.com>
2022-11-15 21:53:29 -0600

    Update scriptIUP.js

--- HDLC/src/JS/scriptIUP.js
+++ HDLC/src/JS/scriptIUP.js
@@ -1,4 +1,4 @@
-onst body = document.querySelector("body"),
+const body = document.querySelector("body"),
       sidebar = body.querySelector(".sidebar"),
       toggle = body.querySelector(".toggle"),
       searchBtn = body.querySelector(".search-box"),
