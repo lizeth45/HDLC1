256421e056051c8190ed0d515dc610242f521a9e
k3llydev <hello@k3lly.dev>
2022-11-19 20:44:27 -0600

    Fix: Correction to typo

--- HDLC/src/connection/Conexiondb.php
+++ HDLC/src/connection/Conexiondb.php
@@ -1,9 +1,9 @@
 <?php 
     function conexion(){
-        $host = $ENV["HDLC1_DATABASE_HOST"];
-        $user = $ENV["HDLC1_DATABASE_USER"];
-        $pass = $ENV["HDLC1_DATABASE_PASS"];
-        $db = $ENV["HDLC1_DATABASE_DB"];
+        $host = $_ENV["HDLC1_DATABASE_HOST"];
+        $user = $_ENV["HDLC1_DATABASE_USER"];
+        $pass = $_ENV["HDLC1_DATABASE_PASS"];
+        $db = $_ENV["HDLC1_DATABASE_DB"];
 
         $con = mysqli_connect($host, $user, $pass);
         mysqli_select_db($con, $db);
