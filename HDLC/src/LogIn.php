c7cbf21d667f35e4214dee10558c18a86455def8
k3llydev <hello@k3lly.dev>
2022-11-19 20:47:45 -0600

    Fix: Forced relative URLs on assets

--- HDLC/src/LogIn.php
+++ HDLC/src/LogIn.php
@@ -5,10 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicia sesion</title>
-    <link rel="shortcut icon" href="../Icons/iconpg.png" type="image/x-icon">
+    <link rel="shortcut icon" href="/Icons/iconpg.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    <link rel="stylesheet" type="text/css" href="../CSS/error.css">
+    <link rel="stylesheet" type="text/css" href="/CSS/error.css">
 
     <style>
         body{
@@ -16,7 +16,7 @@
             background: linear-gradient(to right, #F7F9F9, #AED6F1 );
         }
         .bg{
-            background-image: url(../icons/logIn.jpg);
+            background-image: url(/icons/logIn.jpg);
             background-position: center center;
         }
     </style>
@@ -31,7 +31,7 @@
             <!--En la siguiente col estará el form-->
             <div class="col bg-white p-5 rounded-end"> 
                 <div class="text-end">
-                    <img src="../icons/HDLC.png" width="48" alt="">
+                    <img src="/icons/HDLC.png" width="48" alt="">
                 </div>
                 <h2 class="fw-bold text-center py-5">INICIA SESION</h2>
                 <!--LOGIN-->
