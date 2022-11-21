d5f8f6ef7f31efca96476c8bfade8606e6746600
clxsrdev <chema.pfdez@gmail.com>
2022-11-20 23:02:21 -0600

    updated/ctpr-paciente

---
+++ HDLC/src/apis/databaseConnection.php
@@ -0,0 +1,12 @@
+<?php 
+    function connect(){
+        $host="localhost";
+        $user="root";
+        $pass="";
+
+        $bd="hospitaldc";
+        $con=mysqli_connect($host,$user,$pass);
+        mysqli_select_db($con,$bd);
+        return $con;
+    }
+?>
